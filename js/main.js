a9173b50ac2c6aa962e8ef925c47e4b2fda7459b
martijn <35205@ma-web.nl>
2023-03-07 15:30:28 +0100

    tijd en datum zijn toegevoegd en mappen structuur is gemaakt

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//algemene dingen voor tijd en datum
+const vandaag = document.getElementById("date");
+const tijd = document.getElementById("time");
+var today = new Date();
+
+//dag-maand-jaar
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var datum = date;
+console.log(datum);
+vandaag.innerText = datum;
+
+//tijd in uren en minuten
+var time = (today.getHours()) + ":" + today.getMinutes();
+tijd.innerText = time;
+
+
