288a791de17e5c052c50428d798037588eb51190
martijn <35205@ma-web.nl>
2023-03-17 11:48:02 +0100

    js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,48 @@
-//variable voor tijd en datum
-const vandaag = document.getElementById("date");
-// const tijd = document.getElementById("time");
 let today = new Date();
 
 //dag-maand-jaar
-let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-let datum = date;
-console.log(datum);
-vandaag.innerText = datum;
-
-
-let time = (today.getHours()) + ":" + today.getMinutes() + ":" + today.getSeconds();
-wijzigen = document.getElementById("time");
-function tijd() {   
-    today = new Date();
-    time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    wijzigen.innerText = time;
+const vandaag = document.getElementById("date");
+function datum() {
+    let year = today.getFullYear();
+    let month = (today.getMonth() + 1);
+    let day = today.getDate();
     
+    if (month < 10){
+        month = "0" + month
+    }
+
+    if (day < 10){
+        day = "0" + day
+    }
+    
+    let datum =  day + "/" + month + "/" + year;
+    vandaag.innerText = datum;
+}
+setInterval(datum, 1000);
+
+// uur-minuut-seconde
+wijzigen = document.getElementById("time");
+function tijd() {
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+
+    if (hours < 10) {
+        hours = "0" + hours
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds
+    }
+
+    let acarateTime = hours + ":" + minutes + ":" + seconds;
+
+    wijzigen.innerText = acarateTime;
 }
 setInterval(tijd, 1000);
