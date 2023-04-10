029ddd4b9c1259d877bb646599ac5ddd39968f21
BenteKavsek <35726@ma-web.nl>
2023-04-10 14:07:35 +0200

    Grafiek voor water goede kleur+kleine aanpassingen

--- js/waterverbruik.js
+++ js/waterverbruik.js
@@ -1,22 +1,24 @@
-const xValues = ["maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag","zondag"];
-const yValues = [100, 119, 180, 480, 332, 412, 430];
+const xValues = ["Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag"];
+const yValues = [27, 119, 180, 480, 332, 412, 430];
 
 new Chart("waterVerbruik", {
   type: "line",
   data: {
     labels: xValues,
     datasets: [{
-      fill: false,
+      fill: true,
       lineTension: 0.5,
-      backgroundColor: "rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
+      borderColor: "rgba(119,135,119,1)",
       data: yValues
+
     }]
   },
   options: {
-    legend: {display: false},
+    legend: {
+      display: false
+    },
     scales: {
-      yAxes: [{ticks: {min: 100, max:500}}],
+      yAxes: [{ ticks: { min: 0, max: 500 } }],
     }
   }
 });
